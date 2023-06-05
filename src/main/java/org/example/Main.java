81c1367633e278e6276664a5c05d18d9a97e3dcf
tejass <tejass@steepgraph.com>
2023-06-05 13:27:48 +0530

    FC

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static void display(String param){
+        System.out.println("Your parameter is : " + param);
+    }
+}
